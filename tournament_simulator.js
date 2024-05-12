f058f02a65b33867729747456fb30a5db18edf33
Jelbro <Jelbro97@gmail.com>
2024-05-12 19:30:04 +0100

    Add generate_name and check_for_duplicate_name

--- tournament_simulator.js
+++ tournament_simulator.js
@@ -58,10 +58,27 @@ function get_random_int(min, max) {
 	return Math.floor(Math.random() * (MAX_FLOORED - MIN_CEILED) + MIN_CEILED);
 }
 
+function generate_name(name_database, letters, competitors) {
+	while (true) {
+		let generated_name =
+			name_database[get_random_int(0, 15)] + letters[get_random_int(0, 25)];
+		if (check_for_duplicate_name(generated_name, competitors) == true) break;
+	}
+	return generated_name;
+}
+
+function check_for_duplicate_name(generated_name, competitors) {
+	for (player of competitors) {
+		if (generated_name == player.name) {
+			return true;
+		}
+	}
+}
+
 function create_competitors(name_database) {
 	for (let i = 0; i < 16; i++) {
 		competitors[i] = new competitor(
-			name_database[get_random_int(0, 15)] + letters[get_random_int(0, 25)],
+			generate_name(name_database, letters, competitors),
 			get_random_int(0, 2000),
 			null
 		);
