7bb2b549cc784ca99ac38ae717b0f06ae1dfab80
Jelbro <Jelbro97@gmail.com>
2024-05-12 18:25:16 +0100

    Complete create_competitors function

--- tournament_simulator.js
+++ tournament_simulator.js
@@ -1,48 +1,39 @@
-
 let name_database = [
-    "Isabella",
-    "Maxwell",
-    "Ava",
-    "Lucas",
-    "Olivia",
-    "Ethan",
-    "Sophia",
-    "Logan",
-    "Mia",
-    "Noah",
-    "Charlotte",
-    "Jackson",
-    "Emma",
-    "Aiden",
-    "Amelia",
-    "Elijah",
-    "Harper",
-    "James",
-    "Evelyn",
-    "Carter",
-    "Abigail",
-    "Mason",
-    "Emily",
-    "Alexander",
-    "Elizabeth",
-    "Benjamin",
-    "Ella",
-    "William",
-    "Scarlett",
-    "Michael",
-    "Grace"
-]
+		'Isabella',
+		'Maxwell',
+		'Ava',
+		'Lucas',
+		'Olivia',
+		'Ethan',
+		'Sophia',
+		'Logan',
+		'Mia',
+		'Noah',
+		'Charlotte',
+		'Jackson',
+		'Emma',
+		'Aiden',
+		'Amelia',
+		'Elijah',
+	],
+	competitors = [];
 
-function competitor(name, rating, engine) {
-    this.name = name;
-    this.rating = rating;
-    this.seed = seed;
+function competitor(name, rating, seed) {
+	this.name = name;
+	this.rating = rating;
+	this.seed = seed;
 }
 
-
-
 function create_competitors(name_database) {
-    for (let i = 0; i < 16; i++) {
-         
-    }
+	let random_name_number;
+	let random_rating_number;
+	for (let i = 0; i < 16; i++) {
+		random_name_number = Math.random() * 16;
+		random_rating_number = Math.random() * 2000;
+		competitors[i] = new competitor(
+			name_database[random_name_number],
+			random_rating_number,
+			null
+		);
+	}
 }
