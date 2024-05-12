d7fc6146f30bb2aa48082070ad1f68132922c931
Jelbro <Jelbro97@gmail.com>
2024-05-12 18:10:53 +0100

    Add name_database

--- tournament_simulator.js
+++ tournament_simulator.js
@@ -1,4 +1,37 @@
 
+let name_database = [
+    "Isabella",
+    "Maxwell",
+    "Ava",
+    "Lucas",
+    "Olivia",
+    "Ethan",
+    "Sophia",
+    "Logan",
+    "Mia",
+    "Noah",
+    "Charlotte",
+    "Jackson",
+    "Emma",
+    "Aiden",
+    "Amelia",
+    "Elijah",
+    "Harper",
+    "James",
+    "Evelyn",
+    "Carter",
+    "Abigail",
+    "Mason",
+    "Emily",
+    "Alexander",
+    "Elizabeth",
+    "Benjamin",
+    "Ella",
+    "William",
+    "Scarlett",
+    "Michael",
+    "Grace"
+]
 
 function competitor(name, rating, engine) {
     this.name = name;
@@ -7,8 +40,9 @@ function competitor(name, rating, engine) {
 }
 
 
+
 function create_competitors(name_database) {
     for (let i = 0; i < 16; i++) {
-        let 
+         
     }
 }
