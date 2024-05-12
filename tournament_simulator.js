2af52a399eead55df6462a62130fec72cc96f244
Jelbro <Jelbro97@gmail.com>
2024-05-12 18:31:21 +0100

    Add getRandomInt function

--- tournament_simulator.js
+++ tournament_simulator.js
@@ -24,16 +24,27 @@ function competitor(name, rating, seed) {
 	this.seed = seed;
 }
 
+function getRandomInt(min, max) {
+	const MIN_CEILED = Math.ceil(min);
+	const MAX_FLOORED = Math.floor(max);
+	return Math.floor(Math.random() * (MAX_FLOORED - MIN_CEILED) + MIN_CEILED);
+}
+
 function create_competitors(name_database) {
 	let random_name_number;
 	let random_rating_number;
 	for (let i = 0; i < 16; i++) {
-		random_name_number = Math.random() * 16;
-		random_rating_number = Math.random() * 2000;
+		random_name_number = Math.random() * 16 + 1;
+		random_rating_number = Math.random() * 2000 + 1;
 		competitors[i] = new competitor(
 			name_database[random_name_number],
 			random_rating_number,
 			null
 		);
 	}
+}
+
+create_competitors(name_database);
+for (player of competitors) {
+	console.log(player);
 }
